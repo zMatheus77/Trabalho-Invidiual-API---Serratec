e4b905e9808c99e3f9b6f4cb95d89d3a502b48ce
zmatheus77 <matheusslahr@gmail.com>
2026-05-20 23:22:30 -0300

    Inicializa o projeto

---
+++ ong-animais/src/main/java/org/serratec/ong_animais/OngAnimaisApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.ong_animais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngAnimaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngAnimaisApplication.class, args);
+	}
+
+}
---
+++ ong-animais/src/test/java/org/serratec/ong_animais/OngAnimaisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.ong_animais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngAnimaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
